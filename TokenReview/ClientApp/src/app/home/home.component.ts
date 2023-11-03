2a0ad8bb63e4376de1427281bab6f079896b6a4d
Alexei Holgate <alexeih@dontynesystems.com>
2023-11-03 16:56:56 +0000

    Created entry and controller for posting facebook review

--- TokenReview/ClientApp/src/app/home/home.component.ts
+++ TokenReview/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+
+  onPostFacebookReviewButtonClick() {
+    this.http.get(this.baseUrl + 'postfacebookreview', {}).subscribe(response => {
+      console.log(response); // Handle the response from the server
+    });
+  }
 }
---
+++ TokenReview/Controllers/PostFacebookReviewController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenReview.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class PostFacebookReviewController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult PostFacebookReview()
+		{
+			// Your C# code to be executed goes here
+			// You can return a response if needed
+			return Ok(new { message = "C# code executed successfully" });
+		}
+	}
+
+}
