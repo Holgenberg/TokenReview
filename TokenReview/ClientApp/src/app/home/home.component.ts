ed803ed6d3a6ea3bdb4d11268f3e3a47f2f90b0a
Alexei Holgate <alexeih@dontynesystems.com>
2023-11-14 19:55:39 +0000

    Returned ChatGPT Review from C# code to Angular

--- TokenReview/ClientApp/src/app/home/home.component.ts
+++ TokenReview/ClientApp/src/app/home/home.component.ts
@@ -10,8 +10,17 @@ export class HomeComponent {
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 
   onPostFacebookReviewButtonClick() {
-    this.http.get(this.baseUrl + 'postfacebookreview', {}).pipe(timeout(60000)).subscribe(response => {
-      console.log(response); // Handle the response from the server
-    });
+    this.http.get(this.baseUrl + 'postfacebookreview', {})
+      .subscribe({
+        next: (response: any) => {
+          const reviewMessage = response.message; // Assuming the property is named 'message'
+          console.log('Review:', reviewMessage);
+          // You can now use the 'reviewMessage' variable as needed
+        },
+        error: (error: any) => {
+          console.error('Error:', error);
+          // Handle any errors that occurred during the HTTP request
+        }
+      });
   }
 }
